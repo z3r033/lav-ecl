d41896997ed48618370c0496833e6b0b7e55f32e
z3r033 <saad19551945@gmail.com>
2023-05-08 23:47:57 +0200

    modified:   resources/views/components/menu.blade.php

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,120 +1,113 @@
 <nav class="mt-2">
-    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-        data-accordion="false">
+  <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
-        <li class="nav-item {{setMenuClass('home','menu-open')}}">
-            <a href="#" class="nav-link {{setMenuClass('home','active')}}">
-              <i class="nav-icon fas fa-globe-americas fa-lg"></i>
-              <p>
-                Gestion GIS
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{route('home')}}" class="nav-link active">
-                  <i class="nav-icon fas fa-globe-americas"></i>
-                  <p>GIS</p>
-                </a>
-              </li>
-
-              </li>
-            </ul>
+    <li class="nav-item {{setMenuClass('home','menu-open')}}">
+      <a href="#" class="nav-link {{setMenuClass('home','active')}}">
+        <i class="nav-icon fas fa-globe-americas fa-lg"></i>
+        <p>
+          Gestion GIS
+          <i class="right fas fa-angle-left"></i>
+        </p>
+      </a>
+      <ul class="nav nav-treeview">
+        <li class="nav-item">
+          <a href="{{route('home')}}" class="nav-link  {{setMenuClass('home','active')}}">
+            <i class="nav-icon fas fa-globe-americas"></i>
+            <p>GIS</p>
+          </a>
         </li>
 
+    </li>
+  </ul>
+  </li>
 
 
 
 
-        <li class="nav-item {{setMenuClass('users.index','menu-open')}}">
-            <a href="#" class="nav-link {{setMenuClass('users.index','active')}}">
-              <i class=" nav-icon fas fa-user-shield"></i>
-              <p>
-                Gestion des personnels
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item ">
-                <a
-                href="{{ route('users.index') }}"
-                class="nav-link "
-                >
-                  <i class=" nav-icon fas fa-users-cog"></i>
-                  <p>Utilisateurs</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="nav-icon fas fa-users-slash"></i>
-                  <p>équipes</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="nav-icon fas fa-fingerprint"></i>
-                  <p>roles</p>
-                </a>
-              </li>
 
-            </ul>
-        </li>
+  <li class="nav-item {{setMenuClass('users.index','menu-open')}}">
+    <a href="#" class="nav-link {{setMenuClass('users.index','active')}}">
+      <i class=" nav-icon fas fa-user-shield"></i>
+      <p>
+        Gestion des personnels
+        <i class="right fas fa-angle-left"></i>
+      </p>
+    </a>
+    <ul class="nav nav-treeview">
+      <li class="nav-item  ">
+        <a href="{{ route('users.index') }}" class="nav-link {{setMenuClass('users.index','active')}}">
+          <i class=" nav-icon fas fa-users-cog"></i>
+          <p>Utilisateurs</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-users-slash"></i>
+          <p>équipes</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-fingerprint"></i>
+          <p>roles</p>
+        </a>
+      </li>
 
-        <li class="nav-item {{setMenuClass('articles','menu-open')}} {{setMenuClass('type-articles','menu-open')}} ">
-            <a href="#" class="nav-link {{setMenuClass('articles','active')}} {{setMenuClass('type-articles','active')}}">
-                <i class="nav-icon fas fa-cogs"></i>
-                <p>
-                Gestion articles
-                <i class="right fas fa-angle-left"></i>
-                </p>
-            </a>
-            
+    </ul>
+  </li>
 
-            <ul class="nav nav-treeview">
-                <li class="nav-item">
-                    <a href="{{ route('type-articles')}}"
-                        class="nav-link ">
-                    <i class="nav-icon fas fa-cogs"></i>
-                    <p>Type d'articles</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{route('articles')}}"
-                        class="nav-link ">
-                    <i class="nav-icon  fas fa-solar-panel"></i>
-                    <p>Articles</p>
-                    </a>
-                </li>
-                
-            </ul>
-        </li>
+  <li class="nav-item {{setMenuClass('articles','menu-open')}} {{setMenuClass('type-articles','menu-open')}} ">
+    <a href="#" class="nav-link {{setMenuClass('articles','active')}} {{setMenuClass('type-articles','active')}}">
+      <i class="nav-icon fas fa-cogs"></i>
+      <p>
+        Gestion articles
+        <i class="right fas fa-angle-left"></i>
+      </p>
+    </a>
+
+
+    <ul class="nav nav-treeview">
+      <li class="nav-item ">
+        <a href="{{route('articles')}}" class="nav-link {{setMenuClass('articles','active')}}">
+          <i class="nav-icon  fas fa-solar-panel"></i>
+          <p>Articles</p>
+        </a>
+      </li>
+      <li class="nav-item ">
+        <a href="{{ route('type-articles')}}" class="nav-link {{setMenuClass('type-articles','active')}}">
+          <i class="nav-icon fas fa-cogs"></i>
+          <p>Type d'articles</p>
+        </a>
+      </li>
+
+
+    </ul>
+  </li>
 
 
 
-        <li class="nav-item {{setMenuClass('reclamations','menu-open')}} {{setMenuClass('interventions','menu-open')}}">
-            <a href="#" class="nav-link {{setMenuClass('reclamations','active')}} {{setMenuClass('interventions','active')}}">
-                <i class="nav-icon fas fa-lightbulb"></i>
-                <p>
-                Interventions
-                <i class="right fas fa-angle-left"></i>
-                </p>
-            </a>
-            <ul class="nav nav-treeview">
-                <li class="nav-item">
-                    <a href="{{route('reclamations')}}"
-                        class="nav-link ">
-                    <i class="nav-icon fas fa-bullhorn"></i>
-                    <p>Reclamation</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{route('interventions')}}"
-                        class="nav-link ">
-                    <i class="nav-icon fas fa-wrench"></i>
-                    <p>Interventions</p>
-                    </a>
-                </li>
-                
-            </ul>
-        </li>
+  <li class="nav-item {{setMenuClass('reclamations','menu-open')}} {{setMenuClass('interventions','menu-open')}}">
+    <a href="#" class="nav-link {{setMenuClass('reclamations','active')}} {{setMenuClass('interventions','active')}}">
+      <i class="nav-icon fas fa-lightbulb"></i>
+      <p>
+        Interventions
+        <i class="right fas fa-angle-left"></i>
+      </p>
+    </a>
+    <ul class="nav nav-treeview">
+      <li class="nav-item">
+        <a href="{{route('reclamations')}}" class="nav-link  {{setMenuClass('reclamations','active')}} ">
+          <i class="nav-icon fas fa-bullhorn"></i>
+          <p>Reclamation</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="{{route('interventions')}}" class="nav-link  {{setMenuClass('interventions','active')}} ">
+          <i class="nav-icon fas fa-wrench"></i>
+          <p>Interventions</p>
+        </a>
+      </li>
+
+    </ul>
+  </li>
 </nav>
