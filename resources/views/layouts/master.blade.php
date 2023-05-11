1c5b5223d9fcd371dcf2cc12af0a099cc0a04ca4
z3r033 <saad19551945@gmail.com>
2023-05-11 08:12:42 +0100

    modified:   resources/views/layouts/master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,16 +4,16 @@
 
 <head>
 
-   
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>eclairage pubic</title>
+    <title>Eclairage pubic</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script src="https://cdn.jsdelivr.net/npm/ol@v7.3.0/dist/ol.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/ol@v7.3.0/ol.css">
     <link rel="stylesheet" href="{{mix("css/app.css")}}" />
  {{--    <link rel="stylesheet" href="{{mix("css/components/map.css")}}" /> --}}
- 
+
  {{--  --}}
  <style>
 
@@ -24,17 +24,17 @@
  {{--    <script nonce="2a59e5d6-0fa2-4434-8179-8a922fd4a889">
         (function(w,d){!function(dk,dl,dm,dn){dk[dm]=dk[dm]||{};dk[dm].executed=[];dk.zaraz={deferred:[],listeners:[]};dk.zaraz.q=[];dk.zaraz._f=function(dp){return function(){var dq=Array.prototype.slice.call(arguments);dk.zaraz.q.push({m:dp,a:dq})}};for(const dr of["track","set","debug"])dk.zaraz[dr]=dk.zaraz._f(dr);dk.zaraz.init=()=>{var ds=dl.getElementsByTagName(dn)[0],dt=dl.createElement(dn),du=dl.getElementsByTagName("title")[0];du&&(dk[dm].t=dl.getElementsByTagName("title")[0].text);dk[dm].x=Math.random();dk[dm].w=dk.screen.width;dk[dm].h=dk.screen.height;dk[dm].j=dk.innerHeight;dk[dm].e=dk.innerWidth;dk[dm].l=dk.location.href;dk[dm].r=dl.referrer;dk[dm].k=dk.screen.colorDepth;dk[dm].n=dl.characterSet;dk[dm].o=(new Date).getTimezoneOffset();if(dk.dataLayer)for(const dy of Object.entries(Object.entries(dataLayer).reduce(((dz,dA)=>({...dz[1],...dA[1]})))))zaraz.set(dy[0],dy[1],{scope:"page"});dk[dm].q=[];for(;dk.zaraz.q.length;){const dB=dk.zaraz.q.shift();dk[dm].q.push(dB)}dt.defer=!0;for(const dC of[localStorage,sessionStorage])Object.keys(dC||{}).filter((dE=>dE.startsWith("_zaraz_"))).forEach((dD=>{try{dk[dm]["z_"+dD.slice(7)]=JSON.parse(dC.getItem(dD))}catch{dk[dm]["z_"+dD.slice(7)]=dC.getItem(dD)}}));dt.referrerPolicy="origin";dt.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(dk[dm])));ds.parentNode.insertBefore(dt,ds)};["complete","interactive"].includes(dl.readyState)?zaraz.init():dk.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document);
     </script> --}}
-    
+
 </head>
 
 <body class="hold-transition sidebar-mini">
     <div class="wrapper">
-       
+
 
        <x-topnav />
 
-    
-   
+
+
 
         <aside class="main-sidebar sidebar-dark-info elevation-4">
 
@@ -74,7 +74,7 @@
 
         <div class="content-wrapper">
 
-           
+
             <!-- Main content -->
             <div class="content">
                 <div class="container-fluid">
@@ -82,10 +82,10 @@
                         <div class="col-lg-12">
                             @stack('styles')
                               @stack('scripts')
-             
+
 
                             @yield("contenu")
-              
+
                         </div>
 
 
@@ -109,12 +109,12 @@
             reserved.
         </footer>
     </div>
-   
+
   @livewireScripts
     <script src="{{mix('js/app.js')}}"></script>
        <!-- Scripts -->
-   
-   
+
+
 
 
 </body>
