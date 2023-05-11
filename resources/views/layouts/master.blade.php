f17a9491c1acaa97d438ba973130b00629d1c10c
z3r033 <saad19551945@gmail.com>
2023-05-11 08:19:30 +0100

    modified:   resources/views/layouts/master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Eclairage pubic</title>
+    <title>Eclairage pubique</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script src="https://cdn.jsdelivr.net/npm/ol@v7.3.0/dist/ol.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/ol@v7.3.0/ol.css">
