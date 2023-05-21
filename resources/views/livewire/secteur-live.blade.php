a50c24d4ed3ec87d893870d73292de58fc3f359d
z3r033 <saad19551945@gmail.com>
2023-05-21 19:09:15 +0100

    modified:   resources/views/livewire/secteur-live.blade.php

--- resources/views/livewire/secteur-live.blade.php
+++ resources/views/livewire/secteur-live.blade.php
@@ -49,8 +49,12 @@
 
 
         <div class="card-footer">
-            <button type="submit" class="btn btn-info">Enregistrer</button>
-            <button wire:click="goToListReclamation" type="button" class="btn btn-danger">Annuler</button>
+            <button type="submit" class="btn  btn-info" style="color: black;">Enregistrer</button>
+            <button  type="button" @click="opensupportModal = false" style="color: black;" class="btn btn-danger" data-modal-hide="support-modal">Annuler</button>
+            <button type="button" @click="opensupportModal = false" class="close absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="support-modal">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Close modal</span>
+            </button>
         </div>
     </form>
 </div>
